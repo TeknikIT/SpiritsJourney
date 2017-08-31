0d8cdc513fa4758bdfe144a711ad1362415e3fa8
cajoho99 <caho1212@edu.skovde.se>
2017-08-31 21:48:12 +0200

    New torch prefab

---
+++ Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour {
+
+    public float minIntensity;
+    public float maxIntensity;
+
+
+    // Update is called once per frame
+    void Update () {
+        if(Random.Range(0,100) <= 20)
+            gameObject.GetComponent<Light>().intensity = Random.Range(minIntensity, maxIntensity);
+	}
+}
