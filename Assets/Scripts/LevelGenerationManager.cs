5a985b3cac8320b435a390fb197be93999703eec
caho1212 <caho1212@edu.skovde.se>
2017-09-12 10:54:02 +0200

    Add prefab and bounds manager to GenerationManager Class

--- Assets/Scripts/LevelGenerationManager.cs
+++ Assets/Scripts/LevelGenerationManager.cs
@@ -3,12 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerationManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-        LevelGenerationHelper lgh = new LevelGenerationHelper(10, 10);
+    public GameObject[] RoomPrefabs;
+    private LevelGenerationHelper levelGenerationHelper;
+    private Bounds[] roomBounds;
+    // Use this for initialization
+    void Start () {
+        RoomPrefabs = Resources.LoadAll<GameObject>("Prefabs/Rooms");
+        levelGenerationHelper= new LevelGenerationHelper(10, 10);
+        //Place Rooms
+        for(int i = 0; i < RoomPrefabs.Length; i++)
+        {
+            roomBounds[i] = GetChildrenRenderingBounds(RoomPrefabs[i]);
+        }
 	}
-	
+
+
+    Bounds GetChildrenRenderingBounds(GameObject go)
+    {
+        Renderer[] renderers = go.GetComponentsInChildren<Renderer>();
+        if(renderers.Length > 1)
+        {
+            Bounds bounds = renderers[0].bounds;
+            for (int i = 0, ni = renderers.Length; i < ni; i++)
+            {
+                bounds.Encapsulate(renderers[i].bounds);
+            }
+            return bounds;
+        }
+        else
+        {
+            return new Bounds();
+        }
+
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
