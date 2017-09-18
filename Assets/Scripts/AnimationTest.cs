4f29e087fcbb2b4be20dc14446000ea83c3786f8
cajoho99 <caho1212@edu.skovde.se>
2017-09-18 22:14:32 +0200

    Door animations now work

---
+++ Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        Animator anim = GetComponent<Animator>();
+        if (Input.GetKeyDown(KeyCode.A) && anim.GetBool("IsOpen"))
+        {
+            anim.SetBool("IsOpen", false);
+        }
+        else if(Input.GetKeyDown(KeyCode.A) && !anim.GetBool("IsOpen"))
+        {
+            anim.SetBool("IsOpen", true);
+        }
+	}
+}
