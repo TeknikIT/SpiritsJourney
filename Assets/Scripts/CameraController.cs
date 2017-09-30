0fcb30fe34d11d8e693ef98888623d5eed595895
caho1212 <caho1212@edu.skovde.se>
2017-09-30 15:35:00 +0200

    Experimental features Camera and Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float cameraHeight = 20f;
+    public float cameraDistance = 20f;
+    //public float cameraTilt = -40f;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = player.transform.position;
+        pos.y += cameraHeight;
+        pos.z -= cameraDistance;
+        transform.position = pos;
+        transform.LookAt(player.transform);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movSpeed = 3f;
+    public float rotSpeed = 150f;
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * movSpeed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+	}
+}
