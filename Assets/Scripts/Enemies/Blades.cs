7cf0768697aa783cdcb92178ea4d4e91fcaf0e3f
caho1212 <caho1212@edu.skovde.se>
2018-01-12 13:09:20 +0100

    some comments in code

--- Assets/Scripts/Enemies/Blades.cs
+++ Assets/Scripts/Enemies/Blades.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Rotating the blades of the temporary Enemy
+/// and dealing damage to the player
+/// </summary>
 public class Blades : MonoBehaviour {
 
-    public float rotationSpeed = 10f;
-    public int damageCoefficient = 0;
+    public float rotationSpeed = 10f; 
+    public int damageCoefficient = 10;
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,7 +20,7 @@ public class Blades : MonoBehaviour {
     {
         if(other.tag == "Player")
         {
-            PlayerManager.instance.TakeDamage(damageCoefficient);
+            PlayerManager.instance.TakeDamage(damageCoefficient); //Calling the Take damage funkction on the instance of the player
         }
     }
 }
--- Assets/Scripts/Enemies/EnemyManager.cs
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Manages an enemy's health and status
+/// </summary>
 public class EnemyManager : MonoBehaviour {
 
     public int health;
@@ -11,11 +15,18 @@ public class EnemyManager : MonoBehaviour {
         health = 100;
         
     }
+    /// <summary>
+    /// Removes the enemy
+    /// </summary>
     public void Kill()
     {
         room.monsters.Remove(this.gameObject);
         Destroy(gameObject);
     }
+    /// <summary>
+    /// Removes health from the enemy
+    /// </summary>
+    /// <param name="damage">The damage that will be applied</param>
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -24,6 +35,12 @@ public class EnemyManager : MonoBehaviour {
             Kill();
         }
     }
+    /// <summary>
+    /// Removes health and applies knockback to the enemy
+    /// </summary>
+    /// <param name="damage">Damage to be applied</param>
+    /// <param name="knockbackForce">The force of the knockback</param>
+    /// <param name="knockbackDirection">The direction that the knockback should be applied in</param>
     public void TakeDamageWithKnockback(int damage, float knockbackForce, Vector3 knockbackDirection)
     {
         health -= damage;
