ea2c33d149e1903a6ea7cce555872ecb574aabd6
cajoho99 <caho1212@edu.skovde.se>
2018-03-21 23:28:15 +0100

    Slime and Door fix

---
+++ Assets/HubManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubManager : MonoBehaviour {
+
+    #region Singleton
+    public static HubManager instance;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    public void ToDungeon()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
