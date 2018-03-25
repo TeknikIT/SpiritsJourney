63596eaeb55e5168ddbd0a1bea6fd7263fda5a45
cajoho99 <caho1212@edu.skovde.se>
2018-03-25 20:19:18 +0200

    New menu, save load

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Debug.Log("Cancel");
+        }
+	}
+
+
+}
---
+++ Assets/Scripts/Saving/GlobalControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class GlobalControl : MonoBehaviour {
+
+    public GameStatistics savedGameStatistics = new GameStatistics();
+
+    public bool isSceneBeingLoaded = false;
+    public bool hasStartedPlaying = false;
+
+    #region Singleton
+    public static GlobalControl instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+
+    public void SaveData(string filename)
+    {
+        if (!Directory.Exists("Saves"))
+        {
+            Directory.CreateDirectory("Saves");
+        }
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream saveFile = File.Create("Saves/" + filename);
+
+        PlayerManager.instance.character.SaveStatistics();
+        TokenManager.instance.SaveStatistics();
+
+        formatter.Serialize(saveFile, savedGameStatistics);
+
+        saveFile.Close();
+    }
+
+    public void LoadData(string filename)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream saveFile = File.Open(filename, FileMode.Open);
+        savedGameStatistics = (GameStatistics)formatter.Deserialize(saveFile);
+        saveFile.Close();
+    }
+
+    public string[] DisplaySaves()
+    {
+        return Directory.GetFiles("Saves");
+    }
+
+}
